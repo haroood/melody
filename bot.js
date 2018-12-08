1ba9286aca922096b0a87549ba24a39714f2dbf3
haroood <38558797+haroood@users.noreply.github.com>
2018-12-09 00:04:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("عيونها",{type: 'STREAMING'})
+   client.user.setActivity("بعيونها",{type: 'مغرم'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
